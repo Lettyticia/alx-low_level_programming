9ae5752a274387b38aea7160703ab7b49402be68
Lettyticia <muderedeletticia99@gmail.com>
2023-02-16 16:58:49 -0800

    territory does not ake a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 /**
- * main - Entry
- * Return: Always 0
+ * main - programme that prints size of different types of computers
+ * Return 0 (Success)
  */
 int main(void)
 {
-printf("Size of a char: %i byte(s)\n", size of(char));
-printf("Size of an int: %i byte(s)\n", size of(int));
-printf("Size of a long int: %i byte(s)\n", size of(long int))	
-printf("Size of a long long int: %i byte(s)\n", size of(long long int));
-printf("Size of a float: %i byte(s)\n", size of(float));      
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+printf("Size of a char: %lu byte(s)\n", (unsigned long)size of(a));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)size of(b));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)size of(c));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)size of(d));
+printf("Size of float: %lu byte(s)\n", (unsigned long)size of(f));
 return (0);
 }
