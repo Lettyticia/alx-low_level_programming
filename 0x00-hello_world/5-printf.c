d55dba806427eac7f36e549f69a9df0b1858d559
Lettyticia <muderedeletticia99@gmail.com>
2023-02-19 12:00:13 -0800

    print exactly with proper gramma but piece of art outcone

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+
+
 /**
- * main - Entry
- * Return: Always 0
- */
+* main - print the string in the put function
+*
+*
+* this program prints with proper grammar, but the outcome is a piece of art,
+*
+* Return: Always 0 (success)
+*
+*/
+
 int main(void)
 {
-printf("with proper gramma , but tge outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
