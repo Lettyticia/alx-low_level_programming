d3de92a05f42df2d918082bfee13f65b8e655f91
Lettyticia <muderedeletticia99@gmail.com>
2023-03-10 16:36:28 -0800

    C - argc, argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+ines (34 sloc)  516 Bytes
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * main - add int
+ * @argc: argc
+ * @argv: argv
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; j < (int)strlen(argv[i]); j++)
+			{
+				if (isdigit(argv[i][j]) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
+	else
+		printf("%d\n", 0);
+	return (0);
+}
